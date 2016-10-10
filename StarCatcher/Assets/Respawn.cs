0bb9da45c8658ccaebff76308c3c3ea95767cab8
Unknown <jameson.ballard21@gmail.com>
2016-10-10 13:02:01 -0600

    10/10/2016

---
+++ StarCatcher/Assets/Respawn.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+    
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
