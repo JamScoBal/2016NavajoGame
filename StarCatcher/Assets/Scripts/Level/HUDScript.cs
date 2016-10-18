76ecd92730e9c7a266c389cfe522012f23f058da
Unknown <jameson.ballard21@gmail.com>
2016-10-18 11:47:59 -0600

    10/18/2016

---
+++ StarCatcher/Assets/Scripts/Level/HUDScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+    public Button BackButton;
+
+    void Start ()
+    {
+        BackButton = BackButton.GetComponent<Button>();
+    }
+
+    public void BackLevel()
+    {
+        Application.LoadLevel(0);
+    }
+}
---
+++ StarCatcher/Assets/Scripts/Level/PlayerTransition.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTransition : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetInteger("Change", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("Change", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetInteger("Change", 0);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("Change", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetInteger("Change", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetInteger("Change", 0);
+        }
+    }
+}
